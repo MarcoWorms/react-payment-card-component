05eab4ed06871e9e11b0d28ed2b4de2dbfacce42
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-11-15 00:01:21 -0200

    build: add configs

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,144 @@
+const path = require('path')
+const webpack = require('webpack')
+const eslintFormatter = require('react-dev-utils/eslintFormatter')
+const stylelintFormatter = require('./stylelintFormatter')
+const postcssUrlRebase = require('./postcssUrlRebase')
+
+const indexSrc = path.resolve(__dirname, '../src/index.js')
+const appSrc = path.resolve(__dirname, '../src')
+const libSrc = path.resolve(__dirname, '../lib')
+
+process.env.BABEL_ENV = 'production'
+process.env.NODE_ENV = 'production'
+
+module.exports = {
+  context: appSrc,
+  entry: indexSrc,
+  devtool: 'source-map',
+  target: 'web',
+  output: {
+    path: libSrc,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    filename: 'index.js',
+    sourceMapFilename: 'index.js.map',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint'),
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+        include: appSrc,
+      },
+      {
+        test: /\.css$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: stylelintFormatter,
+              plugins: () => [
+                require('stylelint'),
+                require('postcss-sass-each'),
+                require('postcss-mixins'),
+                require('postcss-import'),
+                require('postcss-url')({
+                  url: postcssUrlRebase,
+                }),
+                require('postcss-cssnext')({
+                  features: {
+                    customProperties: {
+                      strict: false,
+                    },
+                  },
+                }),
+              ],
+            },
+            loader: require.resolve('postcss-loader'),
+          },
+        ],
+        include: path.resolve(__dirname, '../src'),
+      },
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+          {
+            test: /\.(js|jsx|mjs)$/,
+            include: appSrc,
+            loader: require.resolve('babel-loader'),
+            options: {
+              compact: true,
+            },
+          },
+          {
+            test: /\.css$/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  modules: 0,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    require('postcss-sass-each'),
+                    require('postcss-import'),
+                    require('postcss-mixins'),
+                    require('postcss-url')({ url: postcssUrlRebase }),
+                    require('postcss-cssnext'),
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            loader: require.resolve('file-loader'),
+            exclude: [/\.js$/, /\.html$/, /\.json$/],
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //     comparisons: false,
+    //   },
+    //   output: {
+    //     comments: false,
+    //     ascii_only: true,
+    //   },
+    //   sourceMap: true,
+    // }),
+  ],
+  externals: {
+   'react': 'react',
+   'react-dom': 'reactDOM'
+  },
+}
